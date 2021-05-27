bbfc7567e432cc54a5e8c0b2531c88d841b1bd44
RoderickStinson <roderick.zh.wang@outlook.com>
2021-05-27 18:01:09 +0800

    完成网络请求动态生成表

--- src/views/People.vue
+++ src/views/People.vue
@@ -1,66 +1,35 @@
 <template>
   <h3>死亡人数统计表</h3>
-
-  <el-table
-      :data="tableData"
-      style="width: 100%">
-          <el-table-column
-              v-for="(item,index) in tableData"
-              :key="index"
-            :prop="columnNameArray[index]"
-            label="编码"
-            width="180">
-          </el-table-column>
-  </el-table>
-
+  <div v-if="tableData.length > 0">
+    <el-table
+        :data="tableData"
+        style="width: 100%">
+      <el-table-column
+          v-for="item in columnNameArray"
+          :key="item"
+          :prop="item"
+          :label="item"
+          width="180">
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
-// import {getPeopleInfo} from "../Utils/api";
+import {getPeopleInfo} from "../Utils/api";
+import {reactive} from "vue";
 
 export default {
   data() {
     return {
-      test: 'id',
       columnNameArray: ["id", "location", "date", "number", "reportingUnit"],
-      tableData: [{
-        id:'001',
-        location:'学二503',
-        date:'2016-05-04-16:44',
-        number:'2',
-        reportingUnit:'bupt'
-      }, {
-        id:'001',
-        location:'学二503',
-        date:'2016-05-04-16:44',
-        number:'2',
-        reportingUnit:'bupt'
-      },{
-        id:'001',
-        location:'学二503',
-        date:'2016-05-04-16:44',
-        number:'2',
-        reportingUnit:'bupt'
-      },{
-        id:'001',
-        location:'学二503',
-        date:'2016-05-04-16:44',
-        number:'2',
-        reportingUnit:'bupt'
-      },{
-        id:'001',
-        location:'学二503',
-        date:'2016-05-04-16:44',
-        number:'2',
-        reportingUnit:'bupt'
-      }]
+      tableData: reactive([])
     }
   },
   created() {
-    // getPeopleInfo().then(res => {
-    //   this.tableData = res
-    //   console.log(res)
-    // })
+    getPeopleInfo().then(res => {
+      this.tableData = res
+    })
   }
 }
 </script>
